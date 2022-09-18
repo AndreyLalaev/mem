27b7a4b03edeeffdc26806f65dd555350fe46b64
Andrew Lalaev <andrey.lalaev@gmail.com>
2022-09-18 21:33:40 +0300

    mem: initial version with read/write support
    Utility with initial support of read/write from/to `/dev/mem` file.
    For tests we create test-file with random data in `/tmp`.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use clap_num::maybe_hex;
+use mem;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, parse(try_from_str=maybe_hex))]
+    address: usize,
+    #[clap(short, long, parse(try_from_str=maybe_hex))]
+    value: Option<u32>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if let Some(val) = args.value {
+        if let Err(err) = mem::write(mem::DEV_MEM, args.address, val) {
+            eprintln!("Error ({}) write {:#X} to {:#X}", err, val, args.address);
+        }
+    } else {
+        match mem::read(mem::DEV_MEM, args.address) {
+            Ok(value) => {
+                println!("{:#X}", value);
+            }
+            Err(err) => {
+                eprintln!("Error ({}) read {:#X} ", err, args.address);
+            }
+        }
+    }
+}
